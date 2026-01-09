fac8a0ad748065cbd83fc2c125b1991849cac138
Cassia Martinelli <martinelli.cassia@gmail.com>
2026-01-09 18:28:59 -0300

    feat: adicionar S3 para persistencia Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket = "12soat-terraform-state-k8s"
+    key    = "infra-k8s/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
